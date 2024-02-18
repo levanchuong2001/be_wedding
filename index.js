236a3f0471210e3e3cb50406957ee38e497fe6b7
chuongle <trumle2k1@gmail.com>
2024-02-18 19:26:38 +0700

    final

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 var bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const client = new MongoClient(
+  "mongodb+srv://trumle2k1:z3cqaIhfuWMPAoIX@cluster0.e4mqe6j.mongodb.net/",
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,16 +31,19 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-console.log(fs.readFileSync("example_file.txt", "utf8"));
 app.post("/", async (req, res) => {
-  fs.appendFile("example_file.txt", JSON.stringify(req.body) + "\n", (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("write file successfully");
-      fs.readFileSync("example_file.txt", "utf8");
-    }
-  });
+  try {
+    await client.connect();
+    await client
+      .db("wedding")
+      .collection("message")
+      .insertOne(req.body, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+      });
+  } finally {
+    await client.close();
+  }
   res.status(200).json({ message: "OK" });
 });
 app.listen(port, () => {
